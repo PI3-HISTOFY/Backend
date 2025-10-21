4a90a4b63128257dea25e329d3e7ec03b8724bdf
valentinaxd <82911472+valentinaxd@users.noreply.github.com>
2025-10-20 20:11:04 -0500

    Restrict password length to 72 characters
    Limit password length to 72 characters in hashing and verification functions.

--- pi3-histofy-backend/app/security.py
+++ pi3-histofy-backend/app/security.py
@@ -10,9 +10,11 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 60  # 1 hora
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str) -> str:
+    plain_password = plain_password[:72]
     return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
+    plain_password = plain_password[:72]
     return pwd_context.verify(plain_password, hashed_password)
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
